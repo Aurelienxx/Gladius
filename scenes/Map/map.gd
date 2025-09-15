84fd03015d477171260c9ae18ace00770fa15786
SamuelLrg <151573504+Samulus59@users.noreply.github.com>
2025-09-15 11:14:13 +0200

    Affichage carte (sprites)

--- scenes/Map/map.gd
+++ scenes/Map/map.gd
@@ -2,6 +2,7 @@ extends Node
 
 const ARRAY_LONG = 50
 const ARRAY_HEIGHT = 30
+const CELL_SIZE = 32
 
 func _ready():
 	var map = generate_map()
@@ -14,8 +15,8 @@ func generate_map() :
 	var array_map = []
 	var rng = RandomNumberGenerator.new()
 	var random_number
-		
 	array_map.resize(ARRAY_HEIGHT)
+	
 	for i in range(ARRAY_HEIGHT): # Pour chaque ligne
 		# Initialisation de la ligne
 		var array_map_line = []
@@ -23,7 +24,7 @@ func generate_map() :
 		
 		# Attribution d'une valeur aléatoire à chaque case de la ligne
 		for j in range(ARRAY_LONG):
-			random_number = rng.randi_range(0,4)
+			random_number = rng.randi_range(0,3)
 			array_map_line[j] = random_number
 			
 		# Ajout de la ligne au tableau complet
@@ -33,5 +34,29 @@ func generate_map() :
 		
 func display_map(array_map):
 	"""Affiche la carte donnée en paramètre"""
-	# Affichage de la carte
+	
+	var tiles = {
+	0: preload("res://assets/sprites/grounds/grass.png"),
+	1: preload("res://assets/sprites/grounds/mud.png"),
+	2: preload("res://assets/sprites/grounds/barb_wire.png"),
+	3: preload("res://assets/sprites/grounds/gaz.png"),
+}
+
+
+	# Affichage de la carte (array)
 	print(array_map)
+	
+	#Affichage de la carte (sprites)
+	for i in range(ARRAY_HEIGHT):
+		for j in range(ARRAY_LONG):
+			var sprite = Sprite2D.new()
+			sprite.texture = tiles[array_map[i][j]]
+			sprite.position = Vector2(j * CELL_SIZE, i * CELL_SIZE)
+
+			var tex_size = sprite.texture.get_size()
+			var scale_factor = CELL_SIZE / tex_size.x
+			sprite.scale = Vector2(scale_factor, scale_factor)
+			
+			add_child(sprite)
+	
+	
