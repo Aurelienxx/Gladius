0b014b12a93c6154cc6851e59b2f8fcc31031724
SamuelLrg <151573504+Samulus59@users.noreply.github.com>
2025-09-15 10:54:24 +0200

    Optimisation du générateur de carte

--- scenes/Map/map.gd
+++ scenes/Map/map.gd
@@ -1,24 +1,37 @@
 extends Node
 
+const ARRAY_LONG = 50
+const ARRAY_HEIGHT = 30
+
 func _ready():
-	# Creation des tableaux et du nombre aléatoire
+	var map = generate_map()
+	display_map(map)
+	
+func generate_map() :
+	"""génère un tableau de 50x30 de valeurs aléatoires comprises entre 0 et 4 inclus"""
+	
+	# Creation du tableau et du nombre aléatoire
 	var array_map = []
-	var array_map_line = []
 	var rng = RandomNumberGenerator.new()
 	var random_number
-	
-	# Mise en forme des tableaux (50x30)
-	array_map_line.resize(50)
-	array_map.resize(30)
-	
-	# Remplissage de chaque ligne de la carte avec le tableau "ligne"
-	array_map.fill(array_map_line)
-	
-	# Attribution d'une valeur aléatoire à chaque case de la carte
-	for i in range(30):
-		for j in range(50):
+		
+	array_map.resize(ARRAY_HEIGHT)
+	for i in range(ARRAY_HEIGHT): # Pour chaque ligne
+		# Initialisation de la ligne
+		var array_map_line = []
+		array_map_line.resize(ARRAY_LONG)
+		
+		# Attribution d'une valeur aléatoire à chaque case de la ligne
+		for j in range(ARRAY_LONG):
 			random_number = rng.randi_range(0,4)
-			array_map[i][j] = random_number
-
+			array_map_line[j] = random_number
+			
+		# Ajout de la ligne au tableau complet
+		array_map[i] = array_map_line
+		
+	return array_map
+		
+func display_map(array_map):
+	"""Affiche la carte donnée en paramètre"""
 	# Affichage de la carte
 	print(array_map)
