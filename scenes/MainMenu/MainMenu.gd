90cd066d87d2ec80001c02dabd51541debb7963d
AtoupikK <edwyn.piquet@gmail.com>
2025-09-18 14:42:51 +0200

    Update boutons, créations paramètres

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -1,18 +1,27 @@
 extends Control
+@onready var buttons: VBoxContainer = $Buttons
+@onready var settings: Panel = $Settings
 
-func _on_setting_button_pressed():
-	$MenuPrincipal.visible = false
-	$MenuSetting.visible = true
+func _ready():
+	settings.visible = false
+	buttons.visible = true
 
 func _on_back_button_pressed():
-	$MenuSetting.visible = false
-	$MenuPrincipal.visible = true
+	settings.visible = false
+	buttons.visible = true
 	
 func _on_start_button_pressed() -> void:
+	#get_tree().change_scene_to_file()
 	print("Starting game...")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
 
-func _on_music_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+func _on_parameters_button_pressed() -> void:
+	buttons.visible = false
+	settings.visible = true
+
+
+func _on_back_settings_pressed() -> void:
+	settings.visible = false
+	buttons.visible = true
