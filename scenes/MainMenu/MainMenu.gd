ae9810c42b95cf300b16dacce9a01b32eef2a2fc
AtoupikK <edwyn.piquet@gmail.com>
2025-09-19 09:31:27 +0200

    MainMenu update
    Ajout du choix de partie (JcJ, JcIA et IAcIA) et changement de la police d'écriture pour tous les textes présents dans le menu principal.
    Il faut attendre que les scènes de jeu soient faites pour finaliser le menu principal (lier les boutons de choix au type de partie)

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -1,27 +1,50 @@
 extends Control
 @onready var buttons: VBoxContainer = $Buttons
 @onready var settings: Panel = $Settings
+@onready var gamechoice: Panel = $GameChoice
 
 func _ready():
 	settings.visible = false
 	buttons.visible = true
+	gamechoice.visible = false
+	
+'''
+Fonctions liées au bouton jouer.
+'''
+func _on_start_button_pressed() -> void:
+	gamechoice.visible = true
+	buttons.visible = false
 
-func _on_back_button_pressed():
+'''
+Fonctions liées aux boutons de choix de jeu.
+'''
+func _on_joueur_contre_joueur_pressed() -> void:
+	#get_tree().change_scene_to_file()
+	pass
+	
+func _on_joueur_contre_ia_pressed() -> void:
+	pass # Replace with function body.
+	
+func _on_ia_contre_ia_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_back_choice_pressed() -> void:
+	gamechoice.visible = false
+	buttons.visible = true
+
+'''
+Fonctions liées au bouton paramètres.
+'''
+func _on_parameters_button_pressed() -> void:
+	settings.visible = true
+	buttons.visible = false
+
+func _on_back_parameters_pressed() -> void:
 	settings.visible = false
 	buttons.visible = true
-	
-func _on_start_button_pressed() -> void:
-	#get_tree().change_scene_to_file()
-	print("Starting game...")
 
+'''
+Fonctions liées au bouton paramètres.
+'''
 func _on_exit_button_pressed():
 	get_tree().quit()
-
-func _on_parameters_button_pressed() -> void:
-	buttons.visible = false
-	settings.visible = true
-
-
-func _on_back_settings_pressed() -> void:
-	settings.visible = false
-	buttons.visible = true
