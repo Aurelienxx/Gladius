ec796744235a5fbff21410b17030efd9c61eaab8
SamuelLrg <151573504+Samulus59@users.noreply.github.com>
2025-09-19 11:30:35 +0200

    corrections

--- scripts/Economy/EconomyManager.gd
+++ scripts/Economy/EconomyManager.gd
@@ -2,17 +2,17 @@ extends Node
 
 var current_money = 100
 var money_gain = 0
-var money_loose = 0
+var money_loss = 0
 var economic_result = 0
 
 func _ready():
 	money_gain = change_money_gain(money_gain, 25)
-	money_loose = change_money_loose(money_loose, 15)
+	money_loss = change_money_loss(money_loss, 15)
 	
 	print("Vous possédez actuellement ", current_money , " argent")
-	print("Au début du tour, vous remporterez ", money_gain , " et perdrez ", money_loose)
+	print("Au début du tour, vous remporterez ", money_gain , " et perdrez ", money_loss)
 	
-	economic_result = change_money_result(money_gain,money_loose)
+	economic_result = change_money_result(money_gain,money_loss)
 	print("Vous aurez donc un résultat de ",economic_result," argent")
 	print()
 
@@ -26,22 +26,21 @@ func _ready():
 	
 func economy_turn(current_money,economic_result) :
 	current_money += economic_result
-	if current_money < 0 :
-		current_money = 0
+	max(current_money,0)
 	return current_money
 	
-func change_money_result(money_gain,money_loose) :
+func change_money_result(money_gain,money_loss) :
 	var economic_result
-	economic_result = money_gain - money_loose
+	economic_result = money_gain - money_loss
 	return economic_result
 	
 func change_money_gain(money_gain,new_gain) :
 	money_gain += new_gain
 	return money_gain
 	
-func change_money_loose(money_loose,new_loose) :
-	money_loose += new_loose
-	return money_loose
+func change_money_loss(money_loss,new_loss) :
+	money_loss += new_loss
+	return money_loss
 	
 func buy_something(current_money,price) :
 	current_money-= price
