4774058846eac5ddd391e4f7a55954e1f9499b4e
Noa66 <noadeletangdevin@gmail.com>
2025-09-19 15:10:10 +0200

    Ajout des clics sur les unité sur le HUD avec vérification par un print

---
+++ scripts/HUD/HUD.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _on_button_inf_pressed() -> void:
+	print("infanterie pressed")
+
+func _on_button_camion_pressed() -> void:
+	print("camion pressed")
+
+func _on_button_artillerie_pressed() -> void:
+	print("artillerie pressed")
+
+func _on_button_tank_pressed() -> void:
+	print("tank pressed")
