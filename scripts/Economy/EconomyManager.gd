8d21fc6cd4f22b6265b4b1bef5393b9b237ec70c
Clement Cotar <clementcotar59@gmail.com>
2025-09-19 14:25:45 +0200

    economy update
    maintenant on peut améliorer la base

--- scripts/Economy/EconomyManager.gd
+++ scripts/Economy/EconomyManager.gd
@@ -1,27 +1,27 @@
 extends Node
 
-var current_money = 100
-var money_gain = 0
-var money_loss = 0
-var economic_result = 0
-
-func _ready():
-	money_gain = change_money_gain(money_gain, 25)
-	money_loss = change_money_loss(money_loss, 15)
-	
-	print("Vous possédez actuellement ", current_money , " argent")
-	print("Au début du tour, vous remporterez ", money_gain , " et perdrez ", money_loss)
-	
-	economic_result = change_money_result(money_gain,money_loss)
-	print("Vous aurez donc un résultat de ",economic_result," argent")
-	print()
-
-	current_money = economy_turn(current_money, economic_result)
-	print("Le tour a commencé, vous possédez maintenant ", current_money, " argent")
-	
-	print()
-	current_money = buy_something(current_money,50)
-	print("Nouvelle unité achetée! Vous possédez maintenant ", current_money, " argent")
+#var current_money = 100
+#var money_gain = 0
+#var money_loss = 0
+#var economic_result = 0
+#
+#func _ready():
+	#money_gain = change_money_gain(money_gain, 25)
+	#money_loss = change_money_loss(money_loss, 15)
+	#
+	#print("Vous possédez actuellement ", current_money , " argent")
+	#print("Au début du tour, vous remporterez ", money_gain , " et perdrez ", money_loss)
+	#
+	#economic_result = change_money_result(money_gain,money_loss)
+	#print("Vous aurez donc un résultat de ",economic_result," argent")
+	#print()
+#
+	#current_money = economy_turn(current_money, economic_result)
+	#print("Le tour a commencé, vous possédez maintenant ", current_money, " argent")
+	#
+	#print()
+	#current_money = buy_something(current_money,50)
+	#print("Nouvelle unité achetée! Vous possédez maintenant ", current_money, " argent")
 	
 	
 func economy_turn(current_money,economic_result) :
--- scripts/Entities/Building/HeadQuarters.gd
+++ scripts/Entities/Building/HeadQuarters.gd
@@ -1,5 +1,35 @@
 extends Building
 class_name HeadQuarters
 
+var current_gain = 0
+
+
+func _ready() : 
+	print("La partie n'a pas commencé, vous gagnez ",current_gain," par tour")
+	upgrade()
+	print("Gain vers niveau ",lv)
+	print("vous gagnez maintenant ",current_gain," par tour")
+	upgrade()
+	print("Gain vers niveau ",lv)
+	print("vous gagnez maintenant ",current_gain," par tour")
+	upgrade()
+	print("Gain vers niveau ",lv)
+	print("vous gagnez maintenant ",current_gain," par tour")
+
 func _init():
-	super("QG", 1, 1000, 30, 10, 9)
+	super("QG", 0, 1000, 30, 0, 0)
+func upgrade():
+	lv=lv+1
+	level_bonus()
+
+func level_bonus():
+	match lv:
+		1:
+			current_gain=EconomyManager.change_money_gain(current_gain,20)
+
+		2:
+			current_gain=EconomyManager.change_money_gain(current_gain,5)
+			attack += 5
+			attack_range += 3
+		3:
+			current_gain=EconomyManager.change_money_gain(current_gain,5)
