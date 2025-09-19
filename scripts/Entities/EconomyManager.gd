6ba53fc38dfd4b2d0fc01f79604d25b219d727df
SamuelLrg <151573504+Samulus59@users.noreply.github.com>
2025-09-19 09:57:56 +0200

    Calculs économie tour

---
+++ scripts/Entities/EconomyManager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+func _ready():
+	var current_money = 100
+	var money_gain = 0
+	var money_loose = 0
+	var economic_result = 0
+	
+	money_gain = change_money_gain(money_gain, 25)
+	money_loose = change_money_loose(money_loose, 15)
+	
+	print("Vous possédez actuellement ", current_money , " argent")
+	print("Au début du tour, vous remporterez ", money_gain , " et perdrez ", money_loose)
+	
+	economic_result = change_money_result(money_gain,money_loose)
+	print("Vous aurez donc un résultat de ",economic_result," argent")
+	print()
+
+	current_money = economy_turn(current_money, economic_result)
+	print("Le tour a commencé, vous possédez maintenant ", current_money, " argent")
+	
+	print()
+	current_money = buy_something(current_money,50)
+	print("Nouvelle unité achetée! Vous possédez maintenant ", current_money, " argent")
+	
+	
+func economy_turn(current_money,economic_result) :
+	current_money += economic_result
+	if current_money < 0 :
+		current_money = 0
+	return current_money
+	
+func change_money_result(money_gain,money_loose) :
+	var economic_result
+	economic_result = money_gain - money_loose
+	return economic_result
+	
+func change_money_gain(money_gain,new_gain) :
+	money_gain += new_gain
+	return money_gain
+	
+func change_money_loose(money_loose,new_loose) :
+	money_loose += new_loose
+	return money_loose
+	
+func buy_something(current_money,price) :
+	current_money-= price
+	return current_money
--- scripts/Entities/Units/tank.gd
+++ scripts/Entities/Units/tank.gd
@@ -2,4 +2,4 @@ extends Unites
 class_name Tank
 
 func _init():
-	super("Tank", 150, 5, 400, 80, 4, 5, preload("res://assets/sprites/icon.svg"))
+	super("Tank", 150, 5, 400, 80, 4, 5, preload("res://assets/sprites/EntitySprite/Units/SpriteTank/Tank1.png"))
