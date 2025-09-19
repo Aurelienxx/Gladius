ec7ebce55c3c5322286d280d32e235d0e97626fe
AtoupikK <edwyn.piquet@gmail.com>
2025-09-19 09:54:19 +0200

    Création écran de fin
    L'écran de fin est crée, il manque juste le résultat qui sera donnée en paramètre lorsque cette scène sera appelée et le chemin vers la scène du menu principal (ce sera ajouté lorsque le projet sera merge dans le main)

---
+++ scenes/EndScreen/EndScreen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menu_pressed() -> void:
+	#get_tree().change_scene_to_file()
+	pass #changer le chemin vers le fichier de la scène du menu principal
