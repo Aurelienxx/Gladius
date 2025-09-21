3959ba3fa84147457716f88dd817590693d813fb
AtoupikK <edwyn.piquet@gmail.com>
2025-09-21 15:33:27 +0200

    Petite update Upgrade HUD

--- scenes/UpgradeHUD/LevelCard.gd
+++ scenes/UpgradeHUD/LevelCard.gd
@@ -1,12 +1,11 @@
 extends Panel
 
+@onready var buttons: Button = $Button
 
+var money = 50.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_button_pressed() -> void:
+	if money >= this.price:
+		print("yes")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func create_card()
