069e4e91b17d2fb176fbcb097b471f206ad4a74b
AtoupikK <edwyn.piquet@gmail.com>
2025-09-22 13:52:47 +0200

    Update UpgradeHUD
    Dynamisation de la card avec la création de la fonction showCard

--- scripts/UpgradeHUD/LevelCard.gd
+++ scripts/UpgradeHUD/LevelCard.gd
@@ -1,8 +1,19 @@
 extends Panel
 
-@onready var buttons: Button = $Button
+@onready var nom: Label = $Name
+@onready var hp: Label = $HBoxContainer/VBoxContainer/HP
+@onready var dmg: Label = $HBoxContainer/VBoxContainer/DMG
+@onready var portee: Label = $HBoxContainer/VBoxContainer/Range
+@onready var goldGen: Label = $HBoxContainer/VBoxContainer2/GoldGeneration
+@onready var bonus: Label = $HBoxContainer/VBoxContainer2/Bonus
+@onready var buildingSprite: Panel = $BuildingSprite
+@onready var priceCost: Label = $Cost
 
-var money = 50.0
-
-func showCard(name: String, hp: int, dmg: int, gold_generation: int, cost: int, level: int, range: int, bonus: String):
-	
+func showCard(buildingName: String, pv: int, degats: int, attack_range: int, gold_generation: int, cost: int, bon: String):
+	nom.text = "Nom : " + buildingName
+	hp.text = "Points de vie : " + str(pv)
+	dmg.text = "Dégâts : " + str(degats)
+	portee.text = "Portée : " +str(attack_range)
+	goldGen.text = "Génération d'or : " + str(gold_generation)
+	bonus.text = "Bonus : " + bon
+	priceCost.text = "Coût : " + str(cost)
