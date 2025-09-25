500d100e7da3282924c70190ccd88eca24aa6c8d
AtoupikK <edwyn.piquet@gmail.com>
2025-09-25 14:22:46 +0200

    Update TankColor

--- scripts/Entities/Units/TankUnit.gd
+++ scripts/Entities/Units/TankUnit.gd
@@ -2,7 +2,7 @@ class_name TankUnit
 extends CharacterBody2D
 
 # Stats de base (communes à toutes les unités de ce type)
-@export var couleur : PointLight2D
+@onready var couleur : PointLight2D = $AnimatedSprite2D/PointLight2D
 @export var cost: int = 150
 @export var maintenance: int = 5
 @export var max_hp: int = 400
@@ -16,13 +16,15 @@ extends CharacterBody2D
 var current_hp: int
 var equipe: int
 
-func _ready():
-	couleur = $AnimatedSprite2D/PointLight2D
-
 func setup(_equipe: int) -> void:
 	equipe = _equipe
 	current_hp = max_hp
 	if (_equipe == 1):
-		couleur.color = Color(0, 0, 1, 0.5)
+		couleur.color = Color(0, 0, 1, 0.75)
+		print("test bleu")
 	else:
-		couleur.color = Color(1, 0, 0, 0.5)
+		couleur.color = Color(1, 0, 0, 0.75)
+		print("test bleu")
+
+func _ready():
+	setup(1)
