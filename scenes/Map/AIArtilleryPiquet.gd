e7e3eaf254c2eff0506d47f49036e1d17f926fe5
AtoupikK <153225675+AtoupikK@users.noreply.github.com>
2025-10-14 11:18:52 +0200

    Création fonction distance et entité la plus roche
    Création d'une fonction pour calculer la distance et une fonction pour récupérer l'entité la plus proche

--- scenes/Map/AIArtilleryPiquet.gd
+++ scenes/Map/AIArtilleryPiquet.gd
@@ -1,18 +1,41 @@
 extends Node
 
-var listeUnites : Array
+var listeUnites : Array = GameState.all_units
 var listeBuildings : Array
 var listeArtilleries : Array
+var listeUnitesEnnemies : Array
+var listePosEnnemies : Array
 
 func _ready() -> void:
 	GlobalSignal.unitDied.connect(refreshLists)
 	GlobalSignal.unitBought.connect(refreshLists)
+	GlobalSignal.new_turn.connect(closestEntityDistance)
 
 func refreshLists(unit, state: bool):
-	listeUnites = GameState.all_units
-	if (unit and state == true):
-		listeArtilleries.append(unit)
-	else:
+	for unite in listeUnites:
+		if unite.equipe != GameState.current_player:
+			listeUnitesEnnemies.append(unite)
+	if (state == true):
+		if (unit.name_Unite == "Artillerie"):
+			listeArtilleries.append(unit)
+	elif (state == false):
 		for artillerie in listeArtilleries:
 			if artillerie == unit:
 				listeArtilleries.erase(artillerie)
+
+func distance(pos1 : Vector2i, pos2 : Vector2i):
+	return sqrt((pos2.x - pos1.x)**2 + (pos2.y - pos1.y)**2)
+
+func closestEntityDistance(pos: Vector2i):
+	var dist = 0
+	var smallestDistance = null
+	var closestEntity
+	for unite in listeUnitesEnnemies:
+		if smallestDistance:
+			dist = distance(pos, unite.global_position)
+			if (distance <= smallestDistance):
+				smallestDistance = distance
+				closestEntity = unite
+		else:
+			smallestDistance = distance(pos, unite.global_position)
+	return closestEntity
