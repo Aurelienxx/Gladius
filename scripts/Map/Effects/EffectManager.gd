32ed444d2e8185a8fe3aa9a44b293d7053ef9ac5
Clement Cotar <clementcotar59@gmail.com>
2025-11-11 10:00:27 +0100

    ajout des bruit d'explosions

--- scripts/Map/Effects/EffectManager.gd
+++ scripts/Map/Effects/EffectManager.gd
@@ -1,9 +1,10 @@
 extends AnimatedSprite2D
 
-func _ready() -> void:
-	self.play()
-	self.animation_finished.connect(_on_finished)
+@onready var exp_snd: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-func _on_finished():
-	self.visible = false
-	queue_free()  
+func _ready() -> void:
+	play()
+func _on_finished() -> void:
+	print("hello")
+	visible = false
+	queue_free()
